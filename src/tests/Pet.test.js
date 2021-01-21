22f107b5fc27d0b180fff15e241db46b310b3a44
muhammedMoussa <muhammedmoussa@hotmail.com>
2021-01-21 23:47:08 +0200

    wip: test config

--- src/tests/Pet.test.js
+++ src/tests/Pet.test.js
@@ -1,14 +1,18 @@
-import Pet from '../app/Pet';
-jest.mock('../app/Pet'); 
+import Purchase from '../app/Purchase';
+jest.mock('../app/Purchase'); 
 
 beforeEach(() => {
-  Pet.mockClear();
+  Purchase.mockClear();
 });
 
 it('We can check if the consumear called the class constructor', () => {
     const mockSayHi = jest.fn();
-    Pet.prototype.sayHi = mockSayHi
+    Purchase.prototype.sell = mockSayHi
+    console.log(Purchase)
 
-    const dog = new Pet('Pitbull');
-    // expect(dog.sayHi()).toBeTruthy('Pitbull');
+    const adamInvoice = new Purchase('Adam', [1]);
+    // const jhonInvoice = new Purchase('Jhon', [mackerel, husky]);
+    adamInvoice.sell();
+    // const dog = new Pet('Pitbull');
+    // expect(adamInvoice.sell()).toBeCalled();
 });
